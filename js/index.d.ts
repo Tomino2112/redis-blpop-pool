44ce3c2118a3b55ed222352ec84b6f2cd429887f
Tomas <dostal.it@gmail.com>
2016-06-10 16:10:38 +0100

    Trying to figure out best way to auto-generate typings file

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,25 +1,31 @@
-export interface IRedisBlpopPoolOptions {
-    maxClients?: number;
-    clientOptions?: IRedisBlpopPoolClientOptions;
-}
-export interface IRedisBlpopPool {
-    registerKey: (key: string, callback: (value: any) => any) => void;
-    removeKey: (key: string) => void;
-}
-export interface IRedisBlpopPoolClientOptions {
-    maxKeys?: number;
-    timeout?: number;
-}
-export interface IRedisBlpopPoolClient {
-    addKey: (key: string, callback: (value: any) => any) => void;
-    removeKey: (key: string) => void;
-}
-export declare class RedisBlpopPool implements IRedisBlpopPool {
-    private _redis;
-    private _options;
-    private _clients;
-    constructor(redisClient: any, options?: IRedisBlpopPoolOptions);
-    registerKey(key: string, callback: (value: any) => any): void;
-    removeKey(key: string): void;
-    private createClient(clientOptions);
+declare module "redis-blpop-pool" {
+    export interface IRedisBlpopPoolOptions {
+        maxClients?:number;
+        clientOptions?:IRedisBlpopPoolClientOptions;
+    }
+    export interface IRedisBlpopPool {
+        registerKey:(key:string, callback:(value:any) => any) => void;
+        removeKey:(key:string) => void;
+    }
+    export interface IRedisBlpopPoolClientOptions {
+        maxKeys?:number;
+        timeout?:number;
+    }
+    export interface IRedisBlpopPoolClient {
+        addKey:(key:string, callback:(value:any) => any) => void;
+        removeKey:(key:string) => void;
+    }
+    export declare class RedisBlpopPool implements IRedisBlpopPool {
+        private _redis;
+        private _options;
+        private _clients;
+
+        constructor(redisClient:any, options?:IRedisBlpopPoolOptions);
+
+        registerKey(key:string, callback:(value:any) => any):void;
+
+        removeKey(key:string):void;
+
+        private createClient(clientOptions);
+    }
 }
