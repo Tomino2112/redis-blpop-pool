be0c430401cbe178fc271390307e51be00b9fbd0
Tomas <dostal.it@gmail.com>
2016-06-10 16:14:39 +0100

    Trying to figure out best way to auto-generate typings file

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+export interface IRedisBlpopPoolOptions {
+    maxClients?: number;
+    clientOptions?: IRedisBlpopPoolClientOptions;
+}
+export interface IRedisBlpopPool {
+    registerKey: (key: string, callback: (value: any) => any) => void;
+    removeKey: (key: string) => void;
+}
+export interface IRedisBlpopPoolClientOptions {
+    maxKeys?: number;
+    timeout?: number;
+}
+export interface IRedisBlpopPoolClient {
+    addKey: (key: string, callback: (value: any) => any) => void;
+    removeKey: (key: string) => void;
+}
+export declare class RedisBlpopPool implements IRedisBlpopPool {
+    private _redis;
+    private _options;
+    private _clients;
+    constructor(redisClient: any, options?: IRedisBlpopPoolOptions);
+    registerKey(key: string, callback: (value: any) => any): void;
+    removeKey(key: string): void;
+    private createClient(clientOptions);
+}
